03cd97cf87142bf578d9ef3521ff9a675bb9b786
mvaysiberg <mvays11@gmail.com>
2020-12-06 21:14:55 -0500

    make file and rest of server

--- Asst3.c
+++ Asst3.c
@@ -8,6 +8,13 @@
 #include <ctype.h>
 #include <math.h>
 
+int isPunctuation(char c);
+char*  handleMessage(char* message, int i, int length);
+char* makeJoke(int i);
+char* makeError(char* error);
+char* makeError(char* error);void chat(int connfd);
+
+
 int main(int argc, char* argv[]){
 	
 	if (argc != 2){
@@ -59,4 +66,184 @@ int main(int argc, char* argv[]){
         close(connfd);
     }
     close(sockfd);
+}
+
+int isPunctuation(char c){
+    return c == '.' || c == '?' || c == '!';
+}
+
+char*  handleMessage(char* message, int i, int length) {
+	if (i == 0 && strstr(message, "|") != NULL){
+		return "M1FT";
+	}
+	else if (i == 0 && strlen(message) !=length){
+		return "M1LN";
+	}
+	else if(i == 0 && strcmp(message, "Who's there?") != 0) {
+		return "M1CT";
+	}
+
+	else if (i == 1 && strstr(message, "|") != NULL){
+		return "M3FT";
+	}
+	else if (i == 1 && strlen(message) !=length){
+		return "M3LN";
+	}
+	else if(i == 1 && strcmp(message, "Dijkstra, who?") != 0) {
+		return "M3CT";
+	}
+
+	else if (i == 2 && strstr(message, "|") != NULL){
+		return "M5FT";
+	}
+	else if (i == 2 && strlen(message) !=length){
+		return "M5LN";
+	}
+	else if(i == 2 && !isPunctuation(message[length -1]) {
+		return "M5CT";
+	}
+	else {
+		return "good";
+	}
+
+} 
+
+char* makeJoke(int i){
+	char* jokeMessage;
+	if (i == 0){
+		jokeMessage = "Knock, knock.";
+	}else if (i == 1){
+		jokeMessage = "Dijkstra.";
+	}else if (i == 2){
+		jokeMessage = "That path was taking too long, so I let myself in.";
+	}
+	int jokeLen = strlen(jokeMessage);
+	char* formatMessage = malloc(3 + 1+ (floor(log10(jokeLen)) + 1) + 1 +  jokeLen + 1 + 1);
+	strcpy(formatMessage, "REG|");
+	itoa(jokeLen, formatMessage + 4, 10);
+	strcat(formatMessage, "|");
+	strcat(formatMessage, jokeMessage);
+	strcat(formatMessage, "|");
+	//maybe need null terminator for strcat (check for makeError malloc as well)
+	return formatMessage;
+	}
+
+char* makeError(char* error){
+    char* formatError = malloc(10);
+    strcpy(formatError,"ERR|");
+    strcat(formatError, error);
+    strcat(formatError, "|");
+    return formatError;
+}
+
+void chat(int connfd){
+	char header[4];
+	header[3] = '\0';
+
+	for(int i =  0; i < 3;++i) {
+		//parse header
+		char* jokeMessage = makeJoke(i);
+		int jokeLen = strlen(jokeMessage);
+		write(connfd, jokeMessage, jokeLen);
+		free(jokeMessage);
+		int received = 0;
+		while (received < 3){
+			int status = read(connfd, header + received, 3 - received);
+			if (status == -1 || status == 0){
+				//connection closed
+				return;
+			}
+				received += status;
+
+		}
+		if(strcmp(header, "ERR") == 0){
+			//handle error
+			char* errMsg = malloc(6);
+			errMsg[5] = '\0';
+			received = 0;
+			while (received < 5){
+				int status = read(connfd, errMsg + received, 5 - received);
+				if (status == -1 || status == 0){
+					return;
+				}
+				received += status;
+			}
+			printf("Received error message: %s\n", errMsg + 1);
+			return;
+		}else if (strcmp(header, "REG") == 0)){
+			int length = 0
+			char digit;
+			int gotPipe = 0;
+			while (1){
+				int got = read(connfd, &digit, 1);
+				if (got == 1){
+					if (!gotPipe && digit != '|'){
+						//format error
+						handleFormatError(connfd, i, 0);
+					}else if (!gotPipe && digit == '|') {
+						gotPipe = 1;
+						//received pipe ending the length
+					}else if (digit == '|'){
+						break;
+					}else if (isdigit(digit)){
+						length *= 10;
+						length += digit - '0';
+					}else{
+						//did not receive integer data in length
+						//format error
+						handleFormatError(connfd, i, 0);
+					}
+				} else if (got == 0 || got == -1){
+					//connection closed
+					return;
+				}
+			}
+			char* message = malloc(length + 1);
+			received = 0;
+			while (received < length){
+				int status = read(connfd, message + received, length - received);
+				//NEED DifFERENT WAY TO CHECK if MSG IS SHORTER THAN LEN
+				//https://stackoverflow.com/questions/30067822/knowing-when-client-has-finished-reading-from-the-socket-in-c 
+				if (status == -1){
+					//connection closed
+					return;
+				}else if (status == 0){
+					break;
+				}
+				received += status;
+			}
+			message[received] = '\0';
+			//INFINITELY BLOCKED NEED TO FIX
+			int got = read(connfd, &digit, 1);
+			if (got == 1){
+				if(digit != '|'){
+					//error (should be pipe to end message)
+					//length error
+					handleFormatError(connfd, i, 1);
+				}
+			}else if (got == -1){
+				//connection closed
+				return;
+			}
+			else if (got == 0){
+				//format error (no last |)
+				handleFormatError(connfd, i, 0);
+			}
+			//check if there is something after the last |
+			got = read(connfd, &digit, 1);
+			if (got == 1){
+				//format error
+				handleFormatError(connfd, i, 0);
+				return;
+			}
+
+			char* status = handleMessage(message, i);
+			if(strcmp(status, "good") != 0) {
+				char* error = makeError(status);
+				write(connfd, error, 9);
+				free(error);
+			}
+
+		}
+	}
 }
---
+++ Makefile
@@ -0,0 +1,5 @@
+all: Asst3.c
+	gcc Asst3.c -o KKJserver
+
+clean:
+	rm KKJserver
